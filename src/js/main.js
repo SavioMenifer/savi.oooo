b3fdd4bea5868fb7d33378ae5023e0b73bf5b015
saviomenifer <saviomenifer@gmail.com>
2022-03-18 16:31:59 +0100

    add interact.js, tailwind

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+import interact from "interactjs";
+
+const position = { x: 0, y: 0 };
+
+interact(".draggable").draggable({
+  listeners: {
+    start(event) {
+      console.log(event.type, event.target);
+    },
+    move(event) {
+      position.x += event.dx;
+      position.y += event.dy;
+
+      event.target.style.transform = `translate(${position.x}px, ${position.y}px)`;
+    },
+  },
+  modifiers: [
+    interact.modifiers.restrict({
+      restriction: "parent",
+      endOnly: true,
+    }),
+  ],
+  inertia: true,
+});
