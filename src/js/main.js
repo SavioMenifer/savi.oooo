08fdc627fe6942b0028595acc3352d27ed5ece00
Savio Menifer <saviomenifer@gmail.com>
2022-03-21 10:53:42 +0100

    add floating-ui for tooltip

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-// manual implementation
+// Chathead implementation
 
 import InertiaDrag from "./inertia-drag";
 
@@ -6,7 +6,6 @@ const resistanceFactor = 0.8;
 const reboundFactor = 0.1;
 const $el = document.querySelector("#dragMe");
 const $container = document.querySelector("#container");
-const $tip = document.querySelector(".tooltip-container");
 
 const inertiaDrag = new InertiaDrag($el);
 const elRect = $el.getBoundingClientRect();
@@ -14,10 +13,19 @@ const offsetLeft = elRect.left;
 const offsetTop = elRect.top;
 const minX = 0;
 const minY = 0;
-const maxX = $container.offsetWidth - $el.offsetWidth;
-const maxY = $container.offsetHeight - $el.offsetHeight;
+var maxX;
+var maxY;
 
-const onMove = function (event) {
+initValues();
+window.onresize = initValues;
+
+function initValues() {
+  maxX = $container.offsetWidth - $el.offsetWidth;
+  maxY = $container.offsetHeight - $el.offsetHeight;
+  onEnd();
+}
+
+function onMove(event) {
   const elRect = $el.getBoundingClientRect();
   const x = event.deltaX + elRect.left - offsetLeft;
   const y = event.deltaY + elRect.top - offsetTop;
@@ -47,11 +55,11 @@ const onMove = function (event) {
     elRect.top -
     offsetTop;
 
-  moveTooltip(x2, y2);
   $el.style.transform = "translate( " + x2 + "px, " + y2 + "px )";
-};
+  moveTooltip();
+}
 
-const onEnd = function () {
+function onEnd() {
   const elRect = $el.getBoundingClientRect();
   const x = elRect.left - offsetLeft;
   const y = elRect.top - offsetTop;
@@ -63,18 +71,64 @@ const onEnd = function () {
   const y2 = reboundY * reboundFactor + elRect.top - offsetTop;
 
   $el.style.transform = "translate( " + x2 + "px, " + y2 + "px )";
-
+  moveTooltip();
+  //checkUnder(x2, y2);
   if (x2 < minX || x2 > maxX || y2 < minY || y2 > maxY)
     if (Math.abs(reboundX) > 1 || Math.abs(reboundY) > 1)
       requestAnimationFrame(onEnd);
-};
+}
 
 inertiaDrag.addEventListener("dragmove", onMove);
 inertiaDrag.addEventListener("inertiamove", onMove);
 inertiaDrag.addEventListener("dragend", onEnd);
 inertiaDrag.addEventListener("inertiaend", onEnd);
 
-const moveTooltip = function (x, y) {
-  //const tipRect = $tip.getBoundingClientRect();
-  $tip.style.transform = "translate( " + x + "px, " + y + "px )";
-};
+// Tooltip implementation
+
+import {
+  computePosition,
+  shift,
+  autoPlacement,
+  arrow,
+  offset,
+} from "@floating-ui/dom";
+
+const $tip = document.querySelector(".tooltip-container");
+const $arrow = document.querySelector("#arrow");
+
+function moveTooltip() {
+  computePosition($el, $tip, {
+    middleware: [
+      shift(),
+      autoPlacement(),
+      offset(10),
+      arrow({
+        element: $arrow,
+      }),
+    ],
+  }).then(({ x, y, placement, middlewareData }) => {
+    $tip.style.transform = "translate( " + x + "px, " + y + "px )";
+
+    const { x: arrowX, y: arrowY } = middlewareData.arrow;
+
+    const staticSide = {
+      top: "bottom",
+      right: "left",
+      bottom: "top",
+      left: "right",
+    }[placement.split("-")[0]];
+
+    Object.assign($arrow.style, {
+      left: arrowX != null ? `${arrowX}px` : "",
+      top: arrowY != null ? `${arrowY}px` : "",
+      [staticSide]: "-4px",
+    });
+  });
+}
+
+// Detect element under chathead
+
+function checkUnder(x, y) {
+  const elem = document.elementFromPoint(x, y);
+  console.log(elem);
+}
