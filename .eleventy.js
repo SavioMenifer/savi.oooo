a68a42e65b6ade57c378c2a3d9d767ee377bfc06
Savio Menifer <saviomenifer@gmail.com>
2022-04-20 23:53:55 +0200

    use eleventy pathprefix for deploy

--- .eleventy.js
+++ .eleventy.js
@@ -34,62 +34,61 @@ module.exports = function (eleventyConfig) {
 
   // image optimization using eleventy-img
 
-  if (process.env.ELEVENTY_ENV) {
-    const ignoredFormats = [".svg"];
-    const ignoredPages = ["about.html"];
+  const ignoredFormats = [".svg"];
+  const ignoredPages = ["about.html"];
 
-    eleventyConfig.addTransform("transform", (content, outputPath) => {
-      function endsWithAny(suffixes, string) {
-        return suffixes.some(function (suffix) {
-          return string.endsWith(suffix);
-        });
-      }
+  eleventyConfig.addTransform("transform", (content, outputPath) => {
+    function endsWithAny(suffixes, string) {
+      return suffixes.some(function (suffix) {
+        return string.endsWith(suffix);
+      });
+    }
 
-      if (outputPath && endsWithAny(ignoredPages, outputPath)) return content;
+    if (outputPath && endsWithAny(ignoredPages, outputPath)) return content;
 
-      if (outputPath && outputPath.endsWith(".html")) {
-        let { document } = parseHTML(content);
+    if (outputPath && outputPath.endsWith(".html")) {
+      let { document } = parseHTML(content);
 
-        const images = [...document.querySelectorAll("img")];
+      const images = [...document.querySelectorAll("img")];
 
-        images.forEach((i, index) => {
-          const src = "./src/" + i.getAttribute("src");
+      images.forEach((i, index) => {
+        const src = "./src/" + i.getAttribute("src");
 
-          const { ext } = path.parse(src);
-          const { dir } = path.parse(i.getAttribute("src"));
-          if (ignoredFormats.includes(ext)) {
-            // use fs.copyFile to manually copy this file to your output dir
-            return;
-          }
+        const { ext } = path.parse(src);
+        const { dir } = path.parse(i.getAttribute("src"));
+        if (ignoredFormats.includes(ext)) {
+          // use fs.copyFile to manually copy this file to your output dir
+          return;
+        }
 
-          const options = {
-            widths: [1600, 1920, null],
-            sizes: "100vw",
-            formats: ["webp", "jpeg"],
-            urlPath: dir,
-            outputDir: "./docs/" + dir,
-            sharpJpegOptions: { quality: 90 },
-            sharpWebpOptions: { quality: 90 },
-            filenameFormat: function (id, src, width, format, options) {
-              const extension = path.extname(src);
-              const name = path.basename(src, extension);
-              return `${name}-${width}w.${format}`;
-            },
-          };
+        const options = {
+          widths: [1600, 1920, null],
+          sizes: "100vw",
+          formats: ["webp", "jpeg"],
+          urlPath: dir,
+          outputDir: "./docs/" + dir,
+          sharpJpegOptions: { quality: 90 },
+          sharpWebpOptions: { quality: 90 },
+          filenameFormat: function (id, src, width, format, options) {
+            const extension = path.extname(src);
+            const name = path.basename(src, extension);
+            return `${name}-${width}w.${format}`;
+          },
+        };
 
-          const meta = Image.statsSync(src, options);
-          const last = meta.jpeg[meta.jpeg.length - 1];
-          if (last.width < 500) return;
+        const meta = Image.statsSync(src, options);
+        const last = meta.jpeg[meta.jpeg.length - 1];
+        if (last.width < 500) return;
 
-          Image(src, options);
-          i.setAttribute("width", last.width);
-          i.setAttribute("height", last.height);
-          if (index !== 0) {
-            i.setAttribute("loading", "lazy");
-            i.setAttribute("decoding", "async");
-          }
+        Image(src, options);
+        i.setAttribute("width", last.width);
+        i.setAttribute("height", last.height);
+        if (index !== 0) {
+          i.setAttribute("loading", "lazy");
+          i.setAttribute("decoding", "async");
+        }
 
-          i.outerHTML = `
+        i.outerHTML = `
           <picture>
             <source type="image/webp" sizes="${
               options.sizes
@@ -99,13 +98,12 @@ module.exports = function (eleventyConfig) {
             }" srcset="${meta.jpeg.map((p) => p.srcset).join(", ")}">
             ${i.outerHTML}
           </picture>`;
-        });
+      });
 
-        return `<!DOCTYPE html>${document.documentElement.outerHTML}`;
-      }
-      return content;
-    });
-  }
+      return `<!DOCTYPE html>${document.documentElement.outerHTML}`;
+    }
+    return content;
+  });
 
   return {
     dir: {
@@ -114,6 +112,5 @@ module.exports = function (eleventyConfig) {
       layouts: "views/layouts",
       includes: "views/components",
     },
-    pathPrefix: "/savi.oooo/",
   };
 };
